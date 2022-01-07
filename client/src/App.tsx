c9427c2822cbc20dd6365a304117e2aeec9aaa1c
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2022-01-06 23:32:31 -0700

    setup mainpage of app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,36 @@
-import './styles.css'
+import twitterLogo from './assets/twitter-logo.svg'
+import './styles/App.css'
+// Constants
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+
+// const OPENSEA_LINK = ''
+// const TOTAL_MINT_COUNT = 50
+
 export const App = () => {
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  )
   return (
-    <h1>
-      TypeScript React - {process.env.NODE_ENV} {process.env.name}{' '}
-    </h1>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">NFT Collection</p>
+          <p className="sub-text">Discover NFT today</p>
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ client/src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module '*.svg' {
+  // eslint-disable-next-line prettier/prettier
+  const content: string
+  export default content
+}
